4fee841e1302c291f21fa38c4443156186880976
mgfernandez3 <mgfernandez3@esei.uvigo.es>
2020-05-05 14:02:29 +0200

    Empezado el add de review

--- handlers/reviews/nueva.py
+++ handlers/reviews/nueva.py
@@ -1,2 +1,24 @@
 # Nueva review
 
+import webapp2
+from webapp2_extras import jinja2
+
+from model.review import Review
+
+
+class NuevaReviewHandler(webapp2.RequestHandler):
+    def get(self):
+
+        valores_plantilla = {
+        }
+
+        jinja = jinja2.get_jinja2(app=self.app)
+        self.response.write(jinja.render_template("nueva_review.html", **valores_plantilla))
+
+    def post(self):
+        pass
+
+
+app = webapp2.WSGIApplication([
+    ('/reviews/nueva', NuevaReviewHandler)
+], debug=True)
