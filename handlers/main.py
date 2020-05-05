fa8b6dc0a7f7df3b69b0f14e887884cf602c5316
mgfernandez3 <mgfernandez3@esei.uvigo.es>
2020-05-05 13:19:16 +0200

    Hecha plantilla index.html Sistema de ficheros reorganizado

--- handlers/main.py
+++ handlers/main.py
@@ -15,11 +15,21 @@
 # limitations under the License.
 #
 import webapp2
+from webapp2_extras import jinja2
+
+from model.review import Review
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        reviews = Review.query().order(-Review.hora)
+
+        valores_plantilla = {
+            "reviews": reviews
+        }
+
+        jinja = jinja2.get_jinja2(app=self.app)
+        self.response.write(jinja.render_template("index.html", **valores_plantilla))
 
 
 app = webapp2.WSGIApplication([
